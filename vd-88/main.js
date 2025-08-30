b9411551b0881dbfc912077fe814f9f1f0ff22f8
IJAHARKHAN <khan7oct@gmail.com>
2025-08-30 12:16:48 +0530

    Get Latest

--- vd-88/main.js
+++ vd-88/main.js
@@ -1,1 +1,20 @@
-console.log("first module system");
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+app.get('/blog/:first', (req, res) => {
+  res.send(`Hello Blog hello ! ${req.params.first}`);
+  console.log(req)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
